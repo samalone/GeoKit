1acc76216a56211437bcf3db8fa0fb29bb880af8
Stuart A. Malone <samalone@llamagraphics.com>
2023-03-04 05:01:53 -0500

    Remove unsafe build flags.

--- Package.swift
+++ Package.swift
@@ -31,8 +31,8 @@ let package = Package(
                 // Enable better optimizations when building in Release configuration. Despite the use of
                 // the `.unsafeFlags` construct required by SwiftPM, this flag is recommended for Release
                 // builds. See <https://github.com/swift-server/guides/blob/main/docs/building.md#building-for-production> for details.
-                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
-                .unsafeFlags(["-symbol-graph-minimum-access-level", "private"])
+//                .unsafeFlags(["-cross-module-optimization"], .when(configuration: .release)),
+//                .unsafeFlags(["-symbol-graph-minimum-access-level", "private"], .when(configuration: .debug))
             ]),
         .testTarget(
             name: "GeoKitTests",
